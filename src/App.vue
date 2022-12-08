221e7ab5bd02a90f83a4be3128d1c7246f113a02
kannan <kannans@datasirpi.com>
2022-12-08 12:30:18 +0530

    event

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="eve">
+    <button @click="showPopup = true">show Popup</button><!--set the showpopup value true-->
+    <PopUp v-show="showPopup" @close="closePopup"/> <!--conditionally render it-->
+  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PopUp from './components/PopUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PopUp,
+   
+  },
+  data() {
+    return {            //add data property to conditionally render it
+      showPopup: false  // show popup initialized to false the popup is hidden on page load 
+    }
+    
+  },
+  methods: {                               //now send the data to the parent with our custom event //now the close event handler to method 
+    closePopup(name) {
+      this.showPopup = false
+      console.log('name', name)
+    }
   }
 }
 </script>
---
+++ src/components/PopUp.vue
@@ -0,0 +1,40 @@
+<!--passing data parent component to child component, some time you might also want to communicate from child component back to  the parent component this is where custome events -->
+<!--use of $emit. events validation (similar to validating props passed into a child component it is also possible to validate custome events that are emitted from the child component )-->
+<!-- to allow components to send 
+and receive events outside beyond the parent-child relationship, producing a result known as the event bus. -->
+<template>
+  <div id="event">
+    <h2 id="pop">This is a Popup</h2> <!--1-->
+    <button id="close" @click="$emit('close','tom')">Close Popup</button>  <!--click event we use event binding-->
+   </div>
+</template>
+ 
+<script>
+export default {
+  name: 'PopUp',
+  emits: ['close']          //that component can emit the parent component(app component) //last step
+    
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+<!--to summarize custom events, specify the emits option in the child component and emit an event using the dollar emit instance property 
+  bind to the custome event using eventbinding in the parent component,
+  any data you want to send from child to the parent  can be included as the second argument to dollar emit -->
